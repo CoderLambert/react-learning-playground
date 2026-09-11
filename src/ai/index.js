cefc04404590598cfa53a23acb15cf763bc33e38
CoderLambert <l156486648@163.com>
2026-09-11 19:15:31 +0800

    fix: make AI client Node ESM compatible

--- src/ai/index.js
+++ src/ai/index.js
@@ -6,12 +6,12 @@ export {
   normalizeChatEvent,
   normalizeHistory,
   serializeContext,
-} from "./contracts";
-export { INITIAL_CHAT_STATE, chatReducer } from "./chatReducer";
-export { ChatStreamParser, parseChatEventStream } from "./streamProtocol";
+} from "./contracts.js";
+export { INITIAL_CHAT_STATE, chatReducer } from "./chatReducer.js";
+export { ChatStreamParser, parseChatEventStream } from "./streamProtocol.js";
 export {
   AiChatAbortError,
   AiChatClientError,
   createAiChatClient,
   getConfiguredAiAssistantUrl,
-} from "./chatClient";
+} from "./chatClient.js";
