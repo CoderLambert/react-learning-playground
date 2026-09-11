b4b8d12888b1c641db1d0893fb936618e2598fd2
CoderLambert <l156486648@163.com>
2026-09-11 16:01:40 +0800

    feat: export workbench state and URL APIs

--- src/workbench/index.js
+++ src/workbench/index.js
@@ -7,3 +7,21 @@ export {
 } from "./constants";
 export { getNoteFileName, getNoteLoader, getNotePath, NOTE_CONTENT_ROOT, NOTE_FILE_SUFFIX, NOTE_PATH_PATTERN } from "./noteRegistry";
 export { toLearningUnit } from "./contracts";
+export {
+  DEFAULT_PERSISTED_WORKBENCH_STATE,
+  clampInspectorWidth,
+  clearPersistedWorkbenchState,
+  getBrowserStorage,
+  persistWorkbenchState,
+  readPersistedWorkbenchState,
+} from "./stateStorage";
+export { usePersistedWorkbenchState } from "./usePersistedWorkbenchState";
+export {
+  DEMO_QUERY_PARAM,
+  buildDemoUrl,
+  getLearningUnitIds,
+  readDemoUrlState,
+  resolveDemoId,
+  useDemoUrlState,
+  writeDemoUrl,
+} from "./demoUrlState";
