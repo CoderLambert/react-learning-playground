2157a263671fb999a9783e3bd9b8a0ddcdfde7b5
CoderLambert <l156486648@gmail.com>
2026-09-18 20:51:50 +0800

    feat(workbench): add guided activity contract

--- src/workbench/public.js
+++ src/workbench/public.js
@@ -7,3 +7,13 @@ export {
   loadRawNote,
 } from "./noteRegistry.js";
 export { toLearningUnit } from "./contracts.js";
+export {
+  assertGuidedActivityDefinition,
+  getGuidedActivity,
+  getGuidedActivityDefinition,
+  GUIDED_RESPONSE_KINDS,
+  GUIDED_REVIEW_RESOURCES,
+  GUIDED_STEP_TYPES,
+  hasGuidedActivity,
+  validateGuidedActivityDefinition,
+} from "./guidedActivity.js";
