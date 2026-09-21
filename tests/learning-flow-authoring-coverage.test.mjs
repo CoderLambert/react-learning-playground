d39c5ea459cdc44aa57bd832b133e39c0f84d0cd
CoderLambert <l156486648@163.com>
2026-09-21 13:30:27 +0800

    test(learning-flow): keep coverage contract one-way

--- tests/learning-flow-authoring-coverage.test.mjs
+++ tests/learning-flow-authoring-coverage.test.mjs
@@ -5,27 +5,12 @@ import {
   SINGLE_LEARNING_FLOW_UNIT_IDS,
   getSingleLearningFlowDefinition,
 } from "../src/learning-flow/learningFlowRegistry.js";
-import {
-  CONCEPT_MODELS,
-  getConceptModelForLearningUnit,
-} from "../src/content/conceptModels.js";
-import {
-  GUIDED_ACTIVITY_DEFINITIONS,
-  getGuidedActivityDefinition,
-} from "../src/workbench/guidedActivity.js";
+import { getConceptModelForLearningUnit } from "../src/content/conceptModels.js";
+import { getGuidedActivityDefinition } from "../src/workbench/guidedActivity.js";
 import { getCanonicalAssessmentQuestions } from "../src/assessment/content/canonicalQuestions.js";
 
 test("every Single Learning Flow unit has complete lesson-owned authoring assets", () => {
   assert.ok(SINGLE_LEARNING_FLOW_UNIT_IDS.length > 0);
-
-  assert.deepEqual(
-    Object.keys(CONCEPT_MODELS).sort(),
-    [...SINGLE_LEARNING_FLOW_UNIT_IDS].sort(),
-  );
-  assert.deepEqual(
-    Object.keys(GUIDED_ACTIVITY_DEFINITIONS).sort(),
-    [...SINGLE_LEARNING_FLOW_UNIT_IDS].sort(),
-  );
 
   for (const learningUnitId of SINGLE_LEARNING_FLOW_UNIT_IDS) {
     const flow = getSingleLearningFlowDefinition(learningUnitId);
