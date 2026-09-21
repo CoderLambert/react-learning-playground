54594a69d0cd560f390880de27a2fa6ed59bb7f5
CoderLambert <l156486648@163.com>
2026-09-21 21:23:45 +0800

    feat(learning-flow): register Batch A Guided activities

--- src/workbench/guidedActivities/index.js
+++ src/workbench/guidedActivities/index.js
@@ -5,6 +5,12 @@ import {
   assertGuidedActivityDefinition,
   validateGuidedActivityDefinition,
 } from "./contract.js";
+import { COMPONENT_JSX_PURE_RENDER_DEFINITION } from "./componentJsxPureRender.js";
+import { PROPS_DEFINITION } from "./props.js";
+import { CHILDREN_DEFINITION } from "./children.js";
+import { MULTI_SLOTS_DEFINITION } from "./multiSlots.js";
+import { CONDITIONAL_RENDERING_DEFINITION } from "./conditionalRendering.js";
+import { PROP_DRILLING_DEFINITION } from "./propDrilling.js";
 import { STATE_SNAPSHOT_QUEUE_DEFINITION } from "./stateSnapshotQueue.js";
 import { RENDERING_LISTS_KEY_DEFINITION } from "./renderingListsKey.js";
 import { PRESERVING_RESETTING_STATE_DEFINITION } from "./preservingResettingState.js";
@@ -26,6 +32,12 @@ function deepFreeze(value, visited = new WeakSet()) {
   return Object.freeze(value);
 }
 
+assertGuidedActivityDefinition(COMPONENT_JSX_PURE_RENDER_DEFINITION);
+assertGuidedActivityDefinition(PROPS_DEFINITION);
+assertGuidedActivityDefinition(CHILDREN_DEFINITION);
+assertGuidedActivityDefinition(MULTI_SLOTS_DEFINITION);
+assertGuidedActivityDefinition(CONDITIONAL_RENDERING_DEFINITION);
+assertGuidedActivityDefinition(PROP_DRILLING_DEFINITION);
 assertGuidedActivityDefinition(STATE_SNAPSHOT_QUEUE_DEFINITION);
 assertGuidedActivityDefinition(RENDERING_LISTS_KEY_DEFINITION);
 assertGuidedActivityDefinition(PRESERVING_RESETTING_STATE_DEFINITION);
@@ -33,12 +45,25 @@ assertGuidedActivityDefinition(NOT_NEED_EFFECT_DEFINITION);
 assertGuidedActivityDefinition(LIFECYCLE_OF_REACTIVE_EFFECTS_DEFINITION);
 
 export const GUIDED_ACTIVITY_DEFINITIONS = deepFreeze({
+  [COMPONENT_JSX_PURE_RENDER_DEFINITION.learningUnitId]: COMPONENT_JSX_PURE_RENDER_DEFINITION,
+  [PROPS_DEFINITION.learningUnitId]: PROPS_DEFINITION,
+  [CHILDREN_DEFINITION.learningUnitId]: CHILDREN_DEFINITION,
+  [MULTI_SLOTS_DEFINITION.learningUnitId]: MULTI_SLOTS_DEFINITION,
+  [CONDITIONAL_RENDERING_DEFINITION.learningUnitId]: CONDITIONAL_RENDERING_DEFINITION,
+  [PROP_DRILLING_DEFINITION.learningUnitId]: PROP_DRILLING_DEFINITION,
   [STATE_SNAPSHOT_QUEUE_DEFINITION.learningUnitId]: STATE_SNAPSHOT_QUEUE_DEFINITION,
   [RENDERING_LISTS_KEY_DEFINITION.learningUnitId]: RENDERING_LISTS_KEY_DEFINITION,
   [PRESERVING_RESETTING_STATE_DEFINITION.learningUnitId]: PRESERVING_RESETTING_STATE_DEFINITION,
   [NOT_NEED_EFFECT_DEFINITION.learningUnitId]: NOT_NEED_EFFECT_DEFINITION,
   [LIFECYCLE_OF_REACTIVE_EFFECTS_DEFINITION.learningUnitId]: LIFECYCLE_OF_REACTIVE_EFFECTS_DEFINITION,
 });
+
+export const COMPONENT_JSX_PURE_RENDER_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["component-jsx-pure-render"];
+export const PROPS_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["props"];
+export const CHILDREN_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["children"];
+export const MULTI_SLOTS_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["multi-slots"];
+export const CONDITIONAL_RENDERING_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["conditional-rendering"];
+export const PROP_DRILLING_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["prop-drilling"];
 
 export const STATE_SNAPSHOT_QUEUE_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["state-snapshot-queue"];
 export const RENDERING_LISTS_KEY_GUIDED_ACTIVITY = GUIDED_ACTIVITY_DEFINITIONS["rendering-lists-key"];
